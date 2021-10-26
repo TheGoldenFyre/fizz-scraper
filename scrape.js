4afbeab6ffc5422cc591f097e3efe15fd889fdc5
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2021-10-26 13:52:52 +0200

    fixed whitespacing

--- scrape.js
+++ scrape.js
@@ -6,9 +6,9 @@ IsAvailable = async () => {
   await page.goto('https://www.the-fizz.com/en/search-nl/#/searchcriteria=BUILDING:FIZZ_UTRECHT;AREA:UTRECHT'); 
   await page.waitForTimeout(8000);
   let r = await page.evaluate(() => {
-      let res = document.getElementsByClassName("search-results")
-      if (res.length > 0) return Promise.resolve(true);
-      else return Promise.resolve(false);
+    let res = document.getElementsByClassName("search-results")
+    if (res.length > 0) return Promise.resolve(true);
+    else return Promise.resolve(false);
   })
   await browser.close();
   return r;
